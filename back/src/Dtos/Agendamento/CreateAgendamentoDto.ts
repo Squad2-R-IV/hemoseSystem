1fd2ea5a1e0ccf1fc968f323e5d67c31413496dd
João Victor Linhares <joao.linhares.dev@gmail.com>
2025-03-09 13:06:34 -0300

    feat: Add Agendamento DTOs and entity for scheduling management

---
+++ back/src/Dtos/Agendamento/CreateAgendamentoDto.ts
@@ -0,0 +1,19 @@
+import { AutoMap } from "@automapper/classes";
+import { TipoAgendamentoEnum, StatusAgendamentoEnum } from "@prisma/client";
+
+export class CreateAgendamentoDto {
+    @AutoMap()
+    id_paciente!: number;
+    @AutoMap()
+    id_funcionario!: string;
+    @AutoMap()
+    data_agendamento!: Date;
+    @AutoMap()
+    hora_agendamento!: Date;
+    @AutoMap()
+    tipo_agendamento!: TipoAgendamentoEnum;
+    @AutoMap()
+    status_agendamento!: StatusAgendamentoEnum;
+    @AutoMap()
+    observacoes?: string;
+}
---
+++ back/src/Dtos/Agendamento/ReadAgendamentoDto.ts
@@ -0,0 +1,21 @@
+import { AutoMap } from "@automapper/classes";
+import { TipoAgendamentoEnum, StatusAgendamentoEnum } from "@prisma/client";
+
+export class ReadAgendamentoDto {
+    @AutoMap()
+    id_agendamento!: number;
+    @AutoMap()
+    id_paciente!: number;
+    @AutoMap()
+    id_funcionario!: string;
+    @AutoMap()
+    data_agendamento!: Date;
+    @AutoMap()
+    hora_agendamento!: Date;
+    @AutoMap()
+    tipo_agendamento!: TipoAgendamentoEnum;
+    @AutoMap()
+    status_agendamento!: StatusAgendamentoEnum;
+    @AutoMap()
+    observacoes?: string;
+}
