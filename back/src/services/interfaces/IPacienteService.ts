507b31c9e4f08b7a162ec758c3d32e07571c59d7
wellbritto98 <eng.jwellbritto@gmail.com>
2025-05-05 19:18:39 -0300

    feat: finc paciente by cpf

--- back/src/services/interfaces/IPacienteService.ts
+++ back/src/services/interfaces/IPacienteService.ts
@@ -2,5 +2,5 @@ import { PacienteEntity } from "../../models/paciente.entity";
 import { IGenericService } from "./IGenericService";
 
 export interface IPacienteService extends IGenericService<PacienteEntity> {
-
+    findPacienteByCpf(cpf: string): Promise<PacienteEntity | null>;
 }
