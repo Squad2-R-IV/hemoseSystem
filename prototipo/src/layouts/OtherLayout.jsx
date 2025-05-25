3a78351fd66c09a3be29d9b12ba1b4408e2be0a0
wellbritto98 <eng.jwellbritto@gmail.com>
2025-05-25 20:16:56 -0300

    deletando arquivos inuties

--- prototipo/src/layouts/OtherLayout.jsx
+++
@@ -1,8 +0,0 @@
-import FallbackLoading from '@/components/FallbackLoading';
-import { Suspense } from 'react';
-const OtherLayout = ({
-  children
-}) => {
-  return <Suspense fallback={<FallbackLoading />}>{children}</Suspense>;
-};
-export default OtherLayout;
--- prototipo/src/types/data.js
+++
@@ -1,5 +0,0 @@
-export const getKanbanSectionVariant = title => {
-  let variant = 'primary';
-  if (title === 'To Do') variant = 'pink';else if (title === 'In Progress') variant = 'warning';else if (title === 'Review') variant = 'success';else if (title === 'Done') variant = 'info';
-  return variant;
-};
