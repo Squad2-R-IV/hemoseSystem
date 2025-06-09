026f9008ecb4a59cd56bb86136a02f492eec48ad
wellbritto98 <127278222+wellbritto98@users.noreply.github.com>
2025-06-09 16:53:00 -0300

    Update header.tsx

--- front/app/components/header/header.tsx
+++ front/app/components/header/header.tsx
@@ -10,7 +10,6 @@ import {
   DropdownMenu,
   Avatar,
   Button,
-  Tooltip,
 } from "@heroui/react";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import Logo from "../../assets/images/logo.svg";
@@ -33,16 +32,14 @@ export default function Header({ onMenuToggle }: HeaderProps) {
   return (
     <Navbar className="shadow-md" maxWidth="full" >
       <NavbarContent justify="start">
-        <Tooltip content="Abrir menu">
-          <Button
-            isIconOnly
-            color="primary"
-            onPress={onMenuToggle}
-            className="text-default-500"
-          >
-            <Bars3Icon className="h-6 w-6 text-white" />
-          </Button>
-        </Tooltip>
+        <Button
+          isIconOnly
+          color="primary"
+          onPress={onMenuToggle}
+          className="text-default-500"
+        >
+          <Bars3Icon className="h-6 w-6 text-white" />
+        </Button>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
@@ -55,26 +52,13 @@ export default function Header({ onMenuToggle }: HeaderProps) {
 
       <NavbarContent as="div" justify="end">
         <Dropdown placement="bottom-end">
-          <Tooltip content="Ações do perfil">
-            <DropdownTrigger>
-              <Button isIconOnly color="primary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-                  />
-                </svg>
-              </Button>
-            </DropdownTrigger>
-          </Tooltip>
+          <DropdownTrigger>
+            <Button isIconOnly color="primary">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+              </svg>
+            </Button>
+          </DropdownTrigger>
           <DropdownMenu aria-label="Profile Actions" variant="flat">
             <DropdownItem key="profile" className="h-14 gap-2">
               <p className="font-semibold">Signed in as</p>
